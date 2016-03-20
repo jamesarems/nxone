a129a01709dece755d9b8e77da7f655054e08ccf
jamesarems <jamesarems@gmail.com>
2016-03-20 08:40:57 +0000

    Beta version .01

--- cloud-install.sh
+++ cloud-install.sh
@@ -1,4 +1,9 @@
-##Opennebula 4.14 Install script
+##Opennebula 4.14 Install script v.01 beta
+##Note
+#Install it with your own risk.
+#This script is in beta stage
+
+
 yum install epel-release -y
 cat << EOT > /etc/yum.repos.d/opennebula.repo
 [opennebula]
@@ -7,7 +12,7 @@ baseurl=http://downloads.opennebula.org/repo/4.14/CentOS/7/x86_64/
 enabled=1
 gpgcheck=0
 EOT
-yum install opennebula-server opennebula-sunstone opennebula-node-kvm -y
+yum install net-tools wget git opennebula-server openssh openssh-server opennebula-sunstone opennebula-node-kvm -y
 echo "\1\ny\n\n" |bash /usr/share/one/install_gems
 
 sed -i 's/:host: 127.0.0.1/:host: 0.0.0.0/g' /etc/one/sunstone-server.conf
@@ -53,4 +58,15 @@ NM_CONTROLLED=no
 
 systemctl restart network.service
 
-cut -c 10-50 one_auth
+PWD='cut -c 10-50 one_auth'
+IP='hostname -i'
+
+echo "*************************************" >> /etc/motd
+echo "Opennebula 4.14 OS by James PS" >> /etc/motd
+echo "https://github.com/jamesarems" >> /etc/motd
+echo "          (c) 2016           " >> /etc/motd
+echo "*************************************" >> /etc/motd
+echo " Username : oneadmin" >> /etc/motd
+echo " Password : $PWD"  >> /etc/motd
+echo " Web UI : http://$IP:9869" >> /etc/motd
+echo "#######################################" >> /etc/motd
