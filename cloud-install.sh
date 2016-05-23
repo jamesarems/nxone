0d1bd3e9bee1b95a5c1c6fab0c0d45bbcf2d64bc
James PS <jamesarems@gmail.com>
2016-05-23 16:06:00 +0400

    OpenNebula 5.0 Beta setup added

--- cloud-install.sh
+++ cloud-install.sh
@@ -108,6 +108,112 @@ echo "######################################################" >> /etc/motd
 #Final message
 clear
 echo "Please reboot your machine to complete this installation"
+#OPennebula 5.0 Beta
+elif [ "$1" == "setup5" ]; then
+clear
+echo "Installing OpenNebula 4.90 / 5.0 Beta"
+sleep 5s
+read -p "Fully Qualified Domain Name to set:" f
+read -p "Your network interface name (eg: eth0 or enp3s0 ) :" g
+
+hostnamectl set-hostname $f
+yum install epel-release -y
+cat << EOT > /etc/yum.repos.d/opennebula.repo
+[opennebula]
+name=opennebula
+baseurl=http://downloads.opennebula.org/repo/5.0/CentOS/7/x86_64/opennebula-4.90.0-1/
+enabled=1
+gpgcheck=0
+EOT
+yum install net-tools gcc sqlite-devel mysql-devel openssl-devel curl-devel rubygem-rake libxml2-devel libxslt-devel patch expat-devel gcc-c++  wget git opennebula-server openssh openssh-server opennebula-sunstone opennebula-node-kvm opennebula-gate opennebula-flow ruby-devel make autoconf -y
+
+echo -e "1\n\n" |/usr/share/one/install_gems
+
+sed -i 's/:host: 127.0.0.1/:host: 0.0.0.0/g' /etc/one/sunstone-server.conf
+#sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+systemctl enable opennebula
+systemctl start opennebula
+chmod +x /etc/rc.d/rc.local
+echo "sh /var/cloud/service.sh"
+mv -f /var/cloud /var/cloud.bak >> /var/log/cloud.log
+mkdir /var/cloud
+touch /var/cloud/service.sh
+chmod +x /var/cloud/service.sh
+systemctl enable opennebula-sunstone
+systemctl start opennebula-sunstone
+
+
+cat << EOT > /var/lib/one/.ssh/config
+Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+EOT
+chown oneadmin:oneadmin /var/lib/one/.ssh/config
+chmod 600 /var/lib/one/.ssh/config
+
+systemctl enable messagebus.service
+systemctl start messagebus.service
+systemctl enable libvirtd.service
+systemctl start libvirtd.service
+systemctl enable nfs.service
+systemctl start nfs.service
+
+##Network Settings
+
+
+touch /etc/sysconfig/network-scripts/ifcfg-br0
+cp /etc/sysconfig/network-scripts/ifcfg-$g /etc/sysconfig/network-scripts/ifcfg-$g.bak
+#cat /dev/null > /etc/sysconfig/network-scripts/ifcfg-$g
+clear
+echo "Copy below content to ifcfg-$g"
+
+echo "
+DEVICE=$g
+BOOTPROTO=none
+NM_CONTROLLED=no
+ONBOOT=yes
+TYPE=Ethernet
+BRIDGE=br0
+" 
+
+echo "Copy below content to /etc/sysconfig/network-scripts/ifcfg-br0"
+
+echo "
+DEVICE=br0
+TYPE=Bridge
+ONBOOT=yes
+BOOTPROTO=dhcp
+NM_CONTROLLED=no
+" 
+read -p "Please hit ENTER after finish copy paste. You must need to copy and paste content to the given location."
+
+systemctl restart network.service
+
+##Adding ssh public key
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCBYlX1NxLs7o++ySQRyuPY5dMdeAIoTh7XGE1Sp5yeaYz7AGegg8ov8jFDf7BtCiwkqboiuxPId38RFYCDLoSjbhzcVzoeMX87b/EcTJP+4DjQqe4lbfNmefK0t7qYPTvlTdK3gQEi9h5uw25RZmo2JqaJ+KoWJqU82es3bBKgEQ== imported-openssh-key" >> /root/.ssh/authorized_keys
+systemctl restart sshd
+cat /dev/null > /etc/motd
+
+PWD=`cut -c 10-50 /var/lib/one/.one/one_auth`
+#IP='hostname -i'
+
+echo "*****************************************************" >> /etc/motd
+echo "       Opennebula 5.0 Beta OS by James PS             " >> /etc/motd
+echo "        https://github.com/jamesarems            " >> /etc/motd
+echo "                 (c) 2016           " >> /etc/motd
+echo "*****************************************************" >> /etc/motd
+echo "    Username : oneadmin   " >> /etc/motd
+echo "    Password : $PWD      "  >> /etc/motd
+echo "    Web UI : http://systemIP:9869     " >> /etc/motd
+echo "    NOTE: If you are not getting network connection, then you have to configure network manually     " >> /etc/motd
+echo "    Details available on https://github.com/jamesarems/opennebula-distro" >> /etc/motd
+echo "######################################################" >> /etc/motd
+
+###
+#Final message
+clear
+echo "Please reboot your machine to complete this installation"
+
 
 elif [ "$1" == "ovs" ] ; then
 clear
